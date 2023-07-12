2c848cb6ada38c3d99c8a96c2b2d2d55a4a12f66
All-Will-Be-One <showmethedarkness@gmail.com>
2023-07-12 16:11:48 +0800

    bug fix and multipage fit

--- Bilibili Blacklist.js
+++ Bilibili Blacklist.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bilibili Blacklist
 // @namespace    http://tampermonkey.net/
-// @version      0.8.0
+// @version      0.8.3
 // @description  不可逆的默认屏蔽首页广告,并在左下角添加了屏蔽词功能,并能自定义每个屏蔽词的范围(真的会有人需要这种自定义吗)
 // @author       Aporia
 // @match        *://*.bilibili.com/*
@@ -46,6 +46,8 @@
         // Save the updated blacklist back to GM storage
         GM_setValue('blacklist', JSON.stringify(blacklist));
     }
+
+
 
     let blockPageTypes = {
         searchPage: {
@@ -93,53 +95,60 @@
     };
 
     let prepareRegex = function() {
-        let pageInfo;
-
         // Find the page info for the current URL
-        for (let pageType in blockPageTypes) {
-            if (window.location.href.includes(blockPageTypes[pageType].urlIncludes)) {
-                pageInfo = blockPageTypes[pageType];
-                pageInfo.name = pageType;
-                break;
-            }
-        }
-
-        if (pageInfo) {
-            // Filter the blacklist to get the keywords that should be active on this page
-            let activeKeywords = blacklist.filter(entry => entry[pageInfo.name])
-            .map(entry => entry.isRegexp ? entry.keyword : entry.keyword.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&'))
-            .join('|');
-
-            if (activeKeywords) {
-                // Create a custom jQuery expression to match elements by regular expression
-                $.expr[":"].containsRegex = $.expr.createPseudo(function(arg) {
-                    var regexp = new RegExp(arg, 'i');
-                    return function(elem) {
-                        return regexp.test($(elem).text());
-                    };
-                });
-                return { pageInfo: pageInfo, containsString: ":containsRegex('" + activeKeywords + "')" };
-            }
-        }
-        return null;
+let pageInfos = [];
+for (let pageType in blockPageTypes) {
+    if (window.location.href.includes(blockPageTypes[pageType].urlIncludes)) {
+        let pageInfo = blockPageTypes[pageType];
+        pageInfo.name = pageType;
+        pageInfos.push(pageInfo);
     }
+}
 
-    let block_blacklist = function(prep){
+let containsStrings = [];
+if (pageInfos.length > 0) {
+    pageInfos.forEach(pageInfo => {
+        // Filter the blacklist to get the keywords that should be active on this page
+        let activeKeywords = blacklist.filter(entry => entry[pageInfo.name])
+        .map(entry => entry.isRegexp ? entry.keyword : entry.keyword.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&'))
+        .join('|');
+        
+        if (activeKeywords) {
+            containsStrings.push({
+                pageInfo: pageInfo,
+                containsString: ":containsRegex('" + activeKeywords + "')"
+            });
+        }
+    });
+        return containsStrings;
+    };
+}
+
+    let block_blacklist = function(prepArray){
         // Block AD
         $("svg.bili-video-card__info--ad").parents("div.bili-video-card").hide();
         $("svg.bili-video-card__info--ad").parents("div.feed-card").hide();
 
-        if (prep) {
-            // Now hide all matching elements on the page
-            prep.pageInfo.matchPairs.forEach(pair => {
-                $(pair.matchSelector + prep.containsString).parents(pair.parentSelector).hide();
+        if (prepArray.length > 0) {
+            prepArray.forEach(prep => {
+                //console.log(prep.pageInfo.name)
+    
+                // Now hide all matching elements on the page
+                prep.pageInfo.matchPairs.forEach(pair => {
+                    $(pair.matchSelector + prep.containsString).parents(pair.parentSelector).hide();
+                });
+    
+                // Apply CSS modifications
+                if(prep.pageInfo.cssModifications){
+                    $('<style>').prop('type', 'text/css').html(prep.pageInfo.cssModifications).appendTo('head');
+                }
             });
-
-            // Apply CSS modifications
-            if(prep.pageInfo.cssModifications){
-                $('<style>').prop('type', 'text/css').html(prep.pageInfo.cssModifications).appendTo('head');
-            }
         }
+    }
+
+    let reblock_blacklist = function(){
+        let prepArray = prepareRegex();
+        block_blacklist(prepArray);
     }
 
     // Create the floating "B" button
@@ -174,7 +183,7 @@
                 // Save the updated blacklist to GM storage
                 GM_setValue('blacklist', JSON.stringify(blacklist));
 
-                block_blacklist();
+                reblock_blacklist();
             }
         }
     });
@@ -272,7 +281,7 @@
                         // Update the blacklist when the checkbox is toggled
                         entry.isRegexp = this.checked;
                         GM_setValue('blacklist', JSON.stringify(blacklist));
-                        block_blacklist();
+                        reblock_blacklist();
                     }
                 });
 
@@ -306,7 +315,7 @@
                         // Remove this keyword from the modal
                         item.remove();
 
-                        block_blacklist();
+                        reblock_blacklist();
                     }
                 });
 
@@ -321,7 +330,7 @@
                             // Update the blacklist when a checkbox is toggled
                             entry[pageType] = this.checked;
                             GM_setValue('blacklist', JSON.stringify(blacklist));
-                            block_blacklist();
+                            reblock_blacklist();
                         }
                     });
 
@@ -359,19 +368,27 @@
         }
     });
 
-    let prep = prepareRegex();
+    //define containsRegex 
+    $.expr[":"].containsRegex = $.expr.createPseudo(function(arg) {
+        var regexp = new RegExp(arg, 'i');
+        return function(elem) {
+            return regexp.test($(elem).text());
+        };
+    });
+
+    let prepArray = prepareRegex();
     $('body').append(buttonB);
     $('body').append(buttonE);
 
     // Run the initial blacklist block
-    block_blacklist(prep);
+    block_blacklist(prepArray);
 
     let running = false;
     const observer = new MutationObserver(function(mutationsList, observer) {
         if (!running) {
             running = true;
             requestAnimationFrame(function() {
-                block_blacklist(prep);
+                block_blacklist(prepArray);
                 running = false;
             });
         }
